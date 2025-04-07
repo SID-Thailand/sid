fc12bde5ddac9c906d649cbe7dad4896f489ea62
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-04-07 12:52:31 +0300

    feat: added form story

---
+++ components/Form.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useFormStory } from '~/composables/stories/formStory'
+
+interface IProps {
+  title?: string
+  description?: string
+  btnText?: string
+}
+
+defineProps<IProps>()
+const { story } = await useFormStory()
+</script>
+
+<template>
+  <form class="form">
+    <div class="form__wrapper">
+      <legend class="form__title">{{ title }}</legend>
+      <p class="form__description">{{ description }}</p>
+    </div>
+  </form>
+</template>
+
+<style scoped lang="scss"></style>
