d7ae1eb5f640169c4b1c3a12deb03096d61fecc4
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-04-07 11:09:14 +0300

    feat: updates projects and news page folders

---
+++ pages/news/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { pageTransition } from '~/transitions/base'
+
+definePageMeta({
+  pageTransition,
+})
+</script>
+
+<template>
+  <section>News</section>
+</template>
+
+<style scoped lang="scss"></style>
