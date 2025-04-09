95fbc624c3364f790fca2f0734d4e1fb0ae5a49e
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-04-09 22:05:58 +0300

    feat: added styles to project about section

--- components/project/AboutSection.vue
+++ components/project/AboutSection.vue
@@ -10,8 +10,29 @@ defineProps<IProps>()
 
 <template>
   <section class="project-about">
-    <div class="project-about__wrapper container">About</div>
+    <div class="project-about__wrapper container">
+      <h2 class="project-about__text">
+        {{ content?.text }}
+      </h2>
+    </div>
   </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.project-about {
+  background-color: var(--neutral-100);
+  color: var(--basic-black);
+  padding: vw(200) 0;
+
+  @media (max-width: $br1) {
+    padding: 100px 0;
+  }
+}
+
+.project-about__text {
+  text-transform: uppercase;
+  text-align: center;
+  max-width: vw(1336);
+  @include heading-h3;
+}
+</style>
