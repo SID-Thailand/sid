80fefcf562fa7aec5d37e2f27d18f028a5411904
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-04-12 00:29:59 +0300

    feat: added project exterior section

--- components/project/ExteriorSection.vue
+++ components/project/ExteriorSection.vue
@@ -51,15 +51,13 @@ defineProps<IProps>()
     padding-bottom: 60px;
   }
 }
-.project-exterior__wrapper {
-}
-.container {
-}
+
 .project-exterior__title {
   text-transform: uppercase;
   text-align: center;
   @include heading-h1;
 }
+
 .project-exterior__content {
   margin-top: vw(200);
 
@@ -67,17 +65,64 @@ defineProps<IProps>()
     margin-top: 48px;
   }
 }
+
 .project-exterior__text {
+  line-height: 1.2em;
+  max-width: vw(325);
+  width: 100%;
   @include text-t3;
+
+  @media (max-width: $br1) {
+    max-width: 70%;
+  }
 }
+
 .project-exterior__slider {
+  margin-top: vw(90);
+  width: 100vw;
+  overflow-x: auto;
+  padding-right: vw(40);
+  padding-left: vw(40);
+  margin-left: vw(-40);
+
+  @media (max-width: $br1) {
+    margin-top: 24px;
+    padding-right: 32px;
+    padding-left: 32px;
+    margin-left: -32px;
+  }
+
+  @media (max-width: $br3) {
+    padding-right: 16px;
+    padding-left: 16px;
+    margin-left: -16px;
+  }
 }
+
 .project-exterior__list {
   display: flex;
   align-items: flex-end;
+  gap: vw(40);
+  min-width: max-content;
+
+  @media (max-width: $br1) {
+    gap: 16px;
+  }
 }
+
 .project-exterior__item {
+  pointer-events: none;
+  user-select: none;
 }
+
 .project-exterior__img {
+  display: block;
+  height: vw(224);
+  aspect-ratio: 336 / 224;
+
+  @media (max-width: $br1) {
+    height: 109px;
+    aspect-ratio: 164 / 109;
+  }
 }
 </style>
