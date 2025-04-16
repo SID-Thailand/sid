84a87115fea2e216d48330bcc91680fa7f76b085
letmewin22 <lkosteckiy5@gmail.com>
2025-04-16 18:13:05 +0300

    feat: update number color header color

---
+++ components/home/NumberItem.vue
@@ -0,0 +1,74 @@
+<script lang="ts" setup>
+interface IProps {
+  card: iHomeNumberItem
+}
+
+defineProps<IProps>()
+
+const $el = ref<HTMLElement | null>(null)
+
+useDetectHeaderColor($el as Ref<HTMLElement>)
+</script>
+
+<template>
+  <li ref="$el" class="numbers-stat__card">
+    <div class="numbers-stat__info">
+      <h3 class="numbers-stat__number">{{ card?.value }}</h3>
+      <p class="numbers-stat__desc" v-html="card?.description" />
+    </div>
+    <ParallaxImg
+      :src="card?.asset?.filename"
+      :alt="card?.asset?.alt"
+      class="numbers-stat__img"
+    />
+  </li>
+</template>
+
+<style scoped lang="scss">
+.numbers-stat__card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: vw(24);
+  position: relative;
+  padding: vw(16);
+  padding-top: vw(20);
+  background-color: var(--basic-white);
+  width: 100%;
+  height: fit-content;
+  color: var(--basic-black);
+
+  &:nth-child(2),
+  &:nth-child(4) {
+    @media (min-width: $br1) {
+      margin-top: vw(240);
+    }
+  }
+
+  @media (max-width: $br1) {
+    gap: 20px;
+    padding: 16px;
+  }
+}
+
+.numbers-stat__number {
+  @include heading-h2;
+}
+
+.numbers-stat__desc {
+  text-transform: lowercase;
+  margin-top: vw(8);
+  @include text-t1;
+
+  @media (max-width: $br1) {
+    margin-top: 8px;
+  }
+}
+
+.numbers-stat__img {
+  display: block;
+  aspect-ratio: 1;
+  width: 100%;
+  object-fit: cover;
+}
+</style>
