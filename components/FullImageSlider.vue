ca175751243c26c9675413d6e2497500b99399b2
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-04-22 20:16:11 +0300

    feat: added new slider component

---
+++ components/FullImageSlider.vue
@@ -0,0 +1,202 @@
+<script lang="ts" setup>
+import { ArrowLeft, ArrowRight } from 'lucide-vue-next'
+import type { iImage } from '~/types/story'
+
+interface IProps {
+  images: iImage[]
+}
+
+const props = defineProps<IProps>()
+
+const activeImg = ref(0)
+const cursorX = ref(0)
+const cursorY = ref(0)
+const cursorType = ref<'left' | 'right' | null>(null)
+
+const handlePrev = () => {
+  activeImg.value =
+    activeImg.value === 0 ? props.images.length - 1 : activeImg.value - 1
+}
+
+const handleNext = () => {
+  activeImg.value =
+    activeImg.value === props.images.length - 1 ? 0 : activeImg.value + 1
+}
+
+const handleMouseMove = (e: MouseEvent) => {
+  cursorX.value = e.clientX
+  cursorY.value = e.clientY
+}
+
+const setCursor = (type: 'left' | 'right' | null) => {
+  cursorType.value = type
+}
+</script>
+
+<template>
+  <div class="full-slider">
+    <div
+      class="full-slider__wrapper"
+      @mousemove="handleMouseMove"
+      @mouseleave="setCursor(null)"
+    >
+      <ul
+        class="full-slider__list"
+        :style="{ transform: `translateX(-${activeImg * 100}%)` }"
+      >
+        <li v-for="(img, idx) in images" :key="idx" class="full-slider__item">
+          <CustomImage
+            :src="img?.filename"
+            :alt="img?.alt"
+            class="full-slider__img"
+          />
+        </li>
+      </ul>
+
+      <div class="full-slider__btns">
+        <button
+          type="button"
+          class="full-slider__btn"
+          @click="handlePrev"
+          @mouseenter="setCursor('left')"
+          @mouseleave="setCursor(null)"
+        >
+          <ArrowLeft />
+        </button>
+        <button
+          type="button"
+          class="full-slider__btn"
+          @click="handleNext"
+          @mouseenter="setCursor('right')"
+          @mouseleave="setCursor(null)"
+        >
+          <ArrowRight />
+        </button>
+      </div>
+
+      <div
+        v-if="cursorType"
+        class="full-slider__cursor"
+        :class="`full-slider__cursor--${cursorType}`"
+        :style="{ left: `${cursorX}px`, top: `${cursorY}px` }"
+      >
+        <ArrowLeft v-if="cursorType === 'left'" />
+        <ArrowRight v-else />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.full-slider {
+  position: relative;
+  height: 100dvh;
+  width: 100%;
+}
+
+.full-slider__wrapper {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+}
+
+.full-slider__list {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  transition: transform 0.5s ease-in-out;
+  will-change: transform;
+}
+
+.full-slider__item {
+  width: 100%;
+  height: 100%;
+  flex-shrink: 0;
+}
+
+.full-slider__img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.full-slider__btns {
+  position: absolute;
+  top: 0;
+  height: 100%;
+  width: 100%;
+
+  @media (max-width: $br1) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 40px;
+    top: 50%;
+    transform: translateY(-50%);
+    height: fit-content;
+  }
+}
+
+.full-slider__btn {
+  cursor: none;
+  background-color: transparent;
+
+  @media (min-width: $br1) {
+    &:first-child {
+      height: 100%;
+      width: 50%;
+    }
+    &:last-child {
+      height: 100%;
+      width: 50%;
+    }
+
+    svg {
+      display: none;
+    }
+  }
+
+  @media (max-width: $br1) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 60px;
+    height: 60px;
+    background-color: var(--basic-black);
+    color: var(--basic-white);
+    border-radius: 50%;
+
+    svg {
+      width: 30px;
+      height: 30px;
+    }
+  }
+}
+
+.full-slider__cursor {
+  pointer-events: none;
+  position: fixed;
+  transform: translate(-50%, -50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: vw(60);
+  height: vw(60);
+  border-radius: 50%;
+  background: var(--basic-black);
+  color: var(--basic-white);
+  z-index: 50;
+
+  &--left svg,
+  &--right svg {
+    width: vw(30);
+    height: vw(30);
+  }
+
+  @media (max-width: $br1) {
+    display: none;
+  }
+}
+</style>
--- components/current-news/GallerySection.vue
+++ components/current-news/GallerySection.vue
@@ -7,23 +7,17 @@ defineProps<iProps>()
 </script>
 
 <template>
-  <div class="gallery">
-    <CustomImage
-      v-for="img in blok.images"
-      :key="img.id"
-      :src="img.filename"
-      :alt="img.alt"
-      class="gallery__img"
-    />
+  <div v-if="blok?.images" class="gallery">
+    <FullImageSlider :images="blok?.images || []" />
   </div>
 </template>
 
 <style scoped lang="scss">
 .gallery {
-}
-.gallery__img {
-  display: block;
-  width: 700px;
-  height: 700px;
+  margin: vw(100) 0;
+
+  @media (max-width: $br1) {
+    margin: 48px 0;
+  }
 }
 </style>
