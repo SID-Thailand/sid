b6997378bf7251f80023b606e5457d447a773476
letmewin22 <lkosteckiy5@gmail.com>
2025-04-24 13:00:18 +0300

    feat: add base transition

---
+++ components/Revealer.vue
@@ -0,0 +1,18 @@
+<template>
+  <Teleport to="#teleports">
+    <div class="revealer"></div>
+  </Teleport>
+</template>
+
+<style scoped lang="scss">
+.revealer {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--neutral-600);
+  clip-path: inset(0 100% 0 0);
+  z-index: 99;
+}
+</style>
--- transitions/base.ts
+++ transitions/base.ts
@@ -1,22 +1,67 @@
 import type { TransitionProps } from 'vue'
-import { gsap } from '../libs/gsap'
+import gsap from 'gsap'
 import { basicObject } from './basicObject'
 
 export const pageTransition: TransitionProps = {
-  mode: 'out-in',
-  css: false,
-  appear: true,
   onEnter(el, done) {
-    gsap.fromTo(
-      el,
-      { opacity: 0 },
-      { opacity: 1, duration: 0.25, onComplete: done }
-    )
+    console.log('enter')
+
+    const revealer = document.querySelector('.revealer') as HTMLElement
+    if (!revealer) {
+      done()
+      return
+    }
+    const tl = gsap.timeline({
+      onComplete: () => {
+        done()
+
+        tl.kill()
+        revealer.style.display = 'none'
+      },
+    })
+    tl.to(revealer, { opacity: 0, duration: 0.5, ease: 'power2.out' }, 0.5)
 
     basicObject.onEnter()
   },
-  onLeave(el, done) {
-    gsap.to(el, { opacity: 0, duration: 0.25, onComplete: done })
+  async onLeave(el, done) {
+    console.log('leave')
+    const revealer = document.querySelector('.revealer') as HTMLElement
+
+    if (!revealer) {
+      done()
+      return
+    }
+
+    const tl = gsap.timeline({
+      onComplete: done,
+    })
+
+    tl.set(revealer, {
+      clipPath: 'inset(0 0 0 100%)',
+      display: 'block',
+      opacity: 1,
+    })
+
+    tl.fromTo(
+      revealer,
+      { clipPath: 'inset(0 0 0 100%)' },
+      {
+        clipPath: 'inset(0 0 0 0%)',
+        duration: 2.5,
+        ease: 'power4.inOut',
+      },
+      0.1
+    )
+
+    tl.to(
+      el,
+      {
+        x: -400,
+        ease: 'power4.inOut',
+        duration: 2.5,
+      },
+      0
+    )
     basicObject.onLeave()
   },
 }
