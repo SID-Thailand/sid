6e7d1a04cb16a2cd420e25b7d5981d0de3a30562
letmewin22 <lkosteckiy5@gmail.com>
2025-05-15 08:13:55 +0300

    fix: sid loader

--- composables/appState.ts
+++ composables/appState.ts
@@ -1,18 +1,16 @@
 export const useAppState = () => {
-  const config = useRuntimeConfig()
-
   const isInEditor = computed(() => Boolean(useRoute().query._storyblok))
   const isLoaded = useState('inLoaded', () => false)
   const isWaiting = useState('isWaiting', () => false)
   const isMenuOpened = useState('isMenuOpened', () => false)
   const isSlideModalOpened = useState('isSlideModalOpened', () => false)
   const isFormModalOpened = useState('isFormModalOpened', () => false)
-  const isUseLoader = useState(
-    'isUseLoader',
-    () => config.public.env === 'production'
-  )
+  // const isUseLoader = useState(
+  //   'isUseLoader',
+  //   () => config.public.ENVIROMENT === 'production'
+  // )
 
-  // const isUseLoader = useState('isUseLoader', () => true)
+  const isUseLoader = useState('isUseLoader', () => true)
 
   const isFullPage = useState('isFullPage', () => false)
 
